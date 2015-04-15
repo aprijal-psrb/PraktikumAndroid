5dc2aaf63304a5eca37781be6ac89655ad921ac4
Aprijal Pasaribu <aprijal.psrb@gmail.com>
2015-04-15 17:32:36 +0700

    fix salah code

--- app/src/main/java/com/hasyim/pertemuanpertama/BmiActivity.java
+++ app/src/main/java/com/hasyim/pertemuanpertama/BmiActivity.java
@@ -31,31 +31,8 @@ public class BmiActivity extends ActionBarActivity {
                 // TODO Auto-generated method stub
                 int h=Integer.parseInt(tinggi.getText().toString());
                 double hasil = ((h-100)*0.90);
-                viewHasil.setText(String.valueOf(hasil));
+                viewHasil.setText(String.valueOf(hasil)+" Kg");
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_bmi, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
